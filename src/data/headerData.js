90634fc6a9affbaa755cd56f2c723e05fecff90d
chaitrabai25 <chaitrabaijr@gmail.com>
2025-11-02 11:11:37 +0530

    chaitra

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,6 +1,6 @@
 import dp from '../assets/png/dp1.jpeg'
 export const headerData = {
-    name: 'CHAITRA BAI J R',
+    name: 'CHAITRA BAI J Rrrrrrr',
     title: "Hello! I am",
     desciption: "Hello! I’m Chaitra Bai J R, a passionate full-stack developer with a strong focus on continuous learning and problem-solving. I thrive in dynamic environments where I can face new challenges and apply innovative solutions.Driven by curiosity, I’m always eager to learn new technologies and expand my skill set. I believe in creating meaningful, high-impact projects, and I am dedicated to delivering results through efficient, scalable, and well-structured solutions.",
     image: dp,
